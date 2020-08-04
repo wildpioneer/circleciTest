91badf29c40aedff1b66cea5acf09c1792d782a9
wildpioneer <atrostyanko@gmail.com>
2020-08-04 21:19:04 +0300

    Added firefox test

--- src/test/java/org/techmeskills/aqa5/MainPageTest.java
+++ src/test/java/org/techmeskills/aqa5/MainPageTest.java
@@ -2,6 +2,7 @@ package org.techmeskills.aqa5;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
@@ -12,6 +13,17 @@ public class MainPageTest {
     public void openChromePage() {
         System.out.println("Selenium test has been started!");
         driver = new ChromeDriver();
+        driver.get("http://google.com");
+
+        System.out.println(driver.getTitle());
+        Assert.assertEquals(driver.getTitle(), "Google");
+        driver.close();
+    }
+
+    @Test
+    public void openFirefoxPage() {
+        System.out.println("Selenium test has been started!");
+        driver = new FirefoxDriver();
         driver.get("http://google.com");
 
         System.out.println(driver.getTitle());
